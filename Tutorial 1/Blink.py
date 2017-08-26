66c9ef5d18da15f28438014cbf780a3a05dc09d3
tidus747 <ivan.traceur@gmail.com>
2017-08-26 13:15:35 +0000

    Añadimos el tutorial 1 (Código)

---
+++ Tutorial 1/Blink.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+''' Copyright (C) 2017 Iván Rodríguez Méndez
+This software is distributed under the GNU General Public
+Licence (version 3 or later); please refer to the file
+Licence.txt, included with the software, for details.
+'''
+
+# Tutorial 1: Blink con el GPIO 18
+
+#importamos los módulos necesarios para nuestro código 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT) ## GPIO 18 como salida
+
+def blink():
+        print "Ejecución de la función blink..."
+        iteracion = 0
+        while iteracion < 10: ## Segundos que durara la funcion
+                GPIO.output(18, True) ## Enciendo el 18
+                time.sleep(0.5) ## Esperamos 1 segundo
+                GPIO.output(18, False) ## Apago el 18
+                time.sleep(0.5) ## Esperamos 1 segundo
+                iteracion = iteracion + 1 
+                print(iteracion)
+        print "Ejecucion de la función blink finalizada"
+        GPIO.cleanup() ## Hago una limpieza de los GPIO
+
+# Código principal desde el que usamos todas las funciones        
+blink()
