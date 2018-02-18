53a735b3e6f2a414add1517bf04daac0e1c12a48
tidus747 <ivan.traceur@gmail.com>
2018-02-18 13:48:54 +0000

    Añadimos el código del tutorial número 5

---
+++ Tutorial 5/Coche_fantastico.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+''' Copyright (C) 2017 Iván Rodríguez Méndez
+This software is distributed under the GNU General Public
+Licence (version 3 or later); please refer to the file
+Licence.txt, included with the software, for details.
+'''
+# Tutorial creado para el canal de YouTube Piensa 3D
+
+# Tutorial 5: Coche fantástico
+
+#importamos los módulos necesarios para nuestro código 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT) ## GPIO 4 
+GPIO.setup(17, GPIO.OUT) ## GPIO 17 
+GPIO.setup(27, GPIO.OUT) ## GPIO 27 
+GPIO.setup(14, GPIO.OUT) ## GPIO 14 
+GPIO.setup(15, GPIO.OUT) ## GPIO 15 
+
+Leds = np.array
+Leds = [27,17,4,15,14]
+
+
+def Apagar_Leds():
+	for i in range(len(Leds)):
+		GPIO.output(Leds[i],False)
+	return 0
+		
+def Encender_Leds():
+	for i in range(len(Leds)):
+		GPIO.output(Leds[i],True)
+		time.sleep(0.05)
+		GPIO.output(Leds[i],False)
+	for i in range(len(Leds)):
+		GPIO.output(Leds[len(Leds)-i-1],True)
+		time.sleep(0.05)
+		GPIO.output(Leds[len(Leds)-i-1],False)
+	        
+# Código principal desde el que usamos todas las funciones
+Apagar_Leds()
+
+for j in range(20):
+	Encender_Leds()
+
+print("Limpiando la configuración de los GPIO")
+GPIO.cleanup() ## Hago una limpieza de los GPIO
