c8b4298e974b4b999e5c5e9c06980bccec693e4f
tidus747 <ivan.traceur@gmail.com>
2018-08-01 19:54:55 +0100

    Corregimos los DC aplicados al led

--- Tutorial 6/ledRGB.py
+++ Tutorial 6/ledRGB.py
@@ -49,13 +49,13 @@ for j in range(10):
 	Encender_leds(Colores)
 
 # Encendemos el color Cyan
-Encender_mix_rgb(0,200,200, Colores)
+Encender_mix_rgb(0,80,80, Colores)
 time.sleep(3)
 # Encendemos el color Amarillo
-Encender_mix_rgb(200,200,0, Colores)
+Encender_mix_rgb(80,80,0, Colores)
 time.sleep(3)
 # Encendemos el color Magenta
-Encender_mix_rgb(200,0,200, Colores)
+Encender_mix_rgb(80,0,80, Colores)
 time.sleep(3)
 
 
