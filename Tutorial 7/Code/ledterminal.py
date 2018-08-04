c0ed2e13bc0912d77d083b19b3b8cfccdef43e1f
tidus747 <ivan.traceur@gmail.com>
2018-08-04 16:54:30 +0100

    Añadimos el directorio del tutorial 7

---
+++ Tutorial 7/Code/ledterminal.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+''' Copyright (C) 2018 Iván Rodríguez Méndez
+This software is distributed under the GNU General Public
+Licence (version 3 or later); please refer to the file
+Licence.txt, included with the software, for details.
+'''
+# Tutorial creado para el canal de YouTube Piensa 3D
+
+# Tutorial 7: Control de un led por línea de comandos
